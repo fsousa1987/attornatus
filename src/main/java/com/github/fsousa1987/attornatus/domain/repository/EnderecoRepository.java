e0d1e3f0becc741d7a8b3e4e8d7a729b157d136b
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-29 19:45:57 -0300

    ajustes nas classes EnderecoRepository e EnderecoServiceImpl

--- src/main/java/com/github/fsousa1987/attornatus/domain/repository/EnderecoRepository.java
+++ src/main/java/com/github/fsousa1987/attornatus/domain/repository/EnderecoRepository.java
@@ -3,5 +3,10 @@ package com.github.fsousa1987.attornatus.domain.repository;
 import com.github.fsousa1987.attornatus.domain.entity.EnderecoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Long> {
+
+    List<EnderecoEntity> findByPessoaId(Long pessoaId);
+
 }
