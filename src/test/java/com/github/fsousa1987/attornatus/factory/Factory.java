bc8066f072fdb81ba45881b62d5a9da64b13df86
Francisco Sousa <franciscoeds1987@gmail.com>
2023-10-01 16:05:07 -0300

    ajuste na classe Factory para não poder ser mais instanciada

--- src/test/java/com/github/fsousa1987/attornatus/factory/Factory.java
+++ src/test/java/com/github/fsousa1987/attornatus/factory/Factory.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 public class Factory {
 
+    private Factory() {
+    }
+
     public static PessoaResponse createPessoaResponse() {
         return PessoaResponse
                 .builder()
